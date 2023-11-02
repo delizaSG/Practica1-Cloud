468339f4258c0fb55cf96dfffcf4b83821a9406f
delizaSG <80077124+delizaSG@users.noreply.github.com>
2023-11-02 12:49:06 -0600

    Create slow-start.py
    e13 file py

---
+++ files/e13/slow-start.py
@@ -0,0 +1,8 @@
+import time
+
+print("Iniciando la aplicación lenta...")
+time.sleep(30)  # Simula un inicio lento de la aplicación
+print("¡La aplicación lenta se ha iniciado!")
+
+while True:
+    time.sleep(10)
